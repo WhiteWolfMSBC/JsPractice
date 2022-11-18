9ed6eb26d733013d8f1ac7b1bbe0756c4d7105a9
Sachin Suarabh <112817785+WhiteWolfMSBC@users.noreply.github.com>
2022-11-18 11:10:14 +0530

    first commit

---
+++ Coding/index.js
@@ -0,0 +1,7 @@
+
+// const name = "Jesse";
+// const age = 40;
+
+// export {name, age};
+
+export const url ="https://jsonplaceholder.typicode.com/users";
---
+++ Coding/index1.js
@@ -0,0 +1,9 @@
+// import { name, age } from "./index.js";
+
+// let text = "My name is " + name + ", I am " + age + ".";
+
+// document.getElementById("demo").innerHTML = text;
+
+window.onload = function() {
+    yourFunction(param1, param2);
+  };
